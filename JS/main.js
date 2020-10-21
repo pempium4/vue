4060a62caf40229ae6d1df408d2b2f5c6d6825e2
Pempium4 <front.shipper@gmail.com>
2020-10-21 15:21:24 +0300

    Computed. Filters

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,48 @@
+Vue.filter('capitalize', function (value){
+    value = value.toString();
+    return value.replace(/\b\w/g, function (l){
+        return l.toUpperCase();
+    });
+});
+
 new Vue({
     el: '#app',
     data: {
         title: 'Hello World!',
-        style: ''
+        style: '',
+        value: '1',
+        show: true,
+        people: [
+            {name: 'Alex', age: 18},
+            {name: 'Mike', age: 22},
+            {name: 'John', age: 21},
+        ],
+        text: ''
     },
     methods: {
         changeText(){
-            this.title = 'Googbye!'
+            this.title = 'Googbye!';
+        },
+        pow(value){
+            if(value === 'qwerty'){
+                alert("Are you joking me?!");
+            }
+            this.value = value;
+        },
+        /*changeReg(text){
+            this.text = text.toUpperCase();
+        }*/
+    },
+    computed: {
+        doubleValue(){
+            if(this.value !== ''){
+                return Math.pow(this.value, 2);
+            }
+        }
+    },
+    filters: {
+        lowerCase(value){
+            return value.toLowerCase();
         }
     }
 });
